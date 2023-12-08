9e7cdeff50f5027544293fc0fec57aa118eabdc7
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2023-12-08 11:55:15 +0100

    Add loading overlay

--- app/components/troi.client.tsx
+++ app/components/troi.client.tsx
@@ -1,4 +1,5 @@
 import { useTroi } from "~/troi/useTroi.hook";
+import { LoadingOverlay } from "./LoadingOverlay";
 
 interface Props {
   username: string;
@@ -9,6 +10,29 @@ export default function Troi(props: Props) {
   const { troiController, loading, initialized } = useTroi(
     props.username,
     props.password,
+  );
+
+  return (
+    <div>
+      {loading && <LoadingOverlay message={"Please wait..."} />}
+
+      <section className="p-4">
+        <a
+          className="angie-link"
+          href="https://digitalservicebund.atlassian.net/wiki/spaces/DIGITALSER/pages/359301512/Time+Tracking"
+          target="_blank"
+        >
+          Read about how to track your time in confluence
+        </a>
+      </section>
+
+      <section className="mt-8 text-xs text-gray-600">
+        <p>
+          Project not showing up? Make sure it's available in Troi and marked as
+          a "favorite".
+        </p>
+      </section>
+    </div>
   );
 
   return <div>TEST</div>;
