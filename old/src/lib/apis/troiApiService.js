332dd4b7b47eabbabfc412310bc8374ab552d510
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2023-12-19 14:18:33 +0100

    Remove old code

--- old/src/lib/apis/troiApiService.js
+++
@@ -1,7 +0,0 @@
-import TroiApiService, { AuthenticationFailed } from "troi-library";
-import { writable } from "svelte/store";
-
-export const troiApi = writable(null);
-
-export default TroiApiService;
-export { AuthenticationFailed };
--- old/src/routes/__layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import "../app.css";
-</script>
-
-<div
-  class="container mx-auto mt-8 w-full max-w-screen-lg text-sm text-gray-800 sm:px-2"
->
-  <slot />
-</div>
