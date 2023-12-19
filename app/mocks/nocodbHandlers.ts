9ae315b0d8b80e8e4ec4568e9087a1979dbea312
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2023-12-19 17:38:04 +0100

    Loading nocodb baseUrl from env for e2e tests

--- app/mocks/nocodbHandlers.ts
+++ app/mocks/nocodbHandlers.ts
@@ -2,7 +2,7 @@ import { HttpResponse, http } from "msw";
 
 export const handlers = [
   http.get(
-    "https://metrics.ds4g.dev:38081/api/v1/db/data/noco/ds4g-data/Tracky-Position-Phase/views/Tracky-Position-Phase",
+    `${process.env.NOCODB_BASE_URL}/api/v1/db/data/noco/ds4g-data/Tracky-Position-Phase/views/Tracky-Position-Phase`,
     ({ request }) => {
       switch (new URL(request.url).searchParams.get("where")) {
         case "(Position ID,eq,4)":
@@ -17,7 +17,7 @@ export const handlers = [
     },
   ),
   http.get(
-    "https://metrics.ds4g.dev:38081/api/v1/db/data/noco/ds4g-data/Tracky-Subproject-Phase/views/Tracky-Subproject-Phase",
+    `${process.env.NOCODB_BASE_URL}/api/v1/db/data/noco/ds4g-data/Tracky-Subproject-Phase/views/Tracky-Subproject-Phase`,
     ({ request }) => {
       switch (new URL(request.url).searchParams.get("where")) {
         case "(Subproject ID,eq,14)":
@@ -32,13 +32,13 @@ export const handlers = [
     },
   ),
   http.get(
-    "https://metrics.ds4g.dev:38081/api/v1/db/data/noco/ds4g-data/Tracky-Phase/views/Tracky-Phase",
+    `${process.env.NOCODB_BASE_URL}/api/v1/db/data/noco/ds4g-data/Tracky-Phase/views/Tracky-Phase`,
     () => {
       return HttpResponse.json(require("./stubs/nocodb/Tracky-Phase.json"));
     },
   ),
   http.get(
-    "https://metrics.ds4g.dev:38081/api/v1/db/data/v1/ds4g-data/Tracky-Task",
+    `${process.env.NOCODB_BASE_URL}/api/v1/db/data/v1/ds4g-data/Tracky-Task`,
     () => {
       return HttpResponse.json(require("./stubs/nocodb/Tracky-Task.json"));
     },
