4f8c7ee7e03adc7310734d6f19ae76ce563c834b
Hendrik Schmidt <hendriknas@gmail.com>
2024-02-14 12:15:42 +0100

    Update entry.server.tsx

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -13,7 +13,7 @@ import isbot from "isbot";
 import { renderToPipeableStream } from "react-dom/server";
 
 if (process.env.MOCK_EXTERNAL_APIS && process.env.NODE_ENV !== "production") {
-  const { server } = require("./mocks/node");
+  const { server } = require("./apis/mocks/node");
   server.listen();
 }
 
