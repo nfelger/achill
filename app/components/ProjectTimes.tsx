d78a0569fcaef295acdf23c5031878bd1a0cbd49
Hendrik Schmidt <hendriknas@gmail.com>
2024-02-21 17:47:47 +0100

    Update ProjectTimes HTML

--- app/components/ProjectTimes.tsx
+++ app/components/ProjectTimes.tsx
@@ -21,55 +21,42 @@ export function ProjectTimes({
   disabled = false,
 }: Props) {
   return calculationPositions.map((position) => (
-    <section
-      key={position.id}
-      className="bg-white"
-      data-testid={`project-section-${position.id}`}
-    >
-      <div className="mb-4 container mx-auto">
-        {!projectTimes.some(
-          ({ calculationPosition }) => calculationPosition === position.id,
-        ) ? (
-          <ProjectTimeForm
-            date={selectedDate}
-            calculationPosition={position}
-            recurringTasks={recurringTasks}
-            phaseTasks={phaseTasks}
-            disabled={disabled}
-          />
-        ) : (
-          projectTimes
-            .filter(
-              ({ calculationPosition }) => calculationPosition === position.id,
-            )
-            .map((projectTime) => (
-              <div
-                key={projectTime.id}
-                data-testid={`projectTimeCard-${position.id}`}
-              >
-                <div
-                  data-test="projectTime-form"
-                  className="flex justify-center"
-                >
-                  <div className="block w-full">
-                    <ProjectTimeForm
-                      date={selectedDate}
-                      projectTimeId={projectTime.id}
-                      values={{
-                        hours: convertFloatTimeToHHMM(projectTime.hours),
-                        description: projectTime.description,
-                      }}
-                      recurringTasks={recurringTasks}
-                      phaseTasks={phaseTasks}
-                      calculationPosition={position}
-                      disabled={disabled}
-                    />
-                  </div>
-                </div>
-              </div>
-            ))
-        )}
-      </div>
-    </section>
+    <div key={position.id} className="mb-4 container mx-auto">
+      {!projectTimes.some(
+        ({ calculationPosition }) => calculationPosition === position.id,
+      ) ? (
+        <ProjectTimeForm
+          date={selectedDate}
+          calculationPosition={position}
+          recurringTasks={recurringTasks}
+          phaseTasks={phaseTasks}
+          disabled={disabled}
+        />
+      ) : (
+        projectTimes
+          .filter(
+            ({ calculationPosition }) => calculationPosition === position.id,
+          )
+          .map((projectTime) => (
+            <div
+              key={projectTime.id}
+              data-testid={`projectTimeCard-${position.id}`}
+            >
+              <ProjectTimeForm
+                date={selectedDate}
+                projectTimeId={projectTime.id}
+                values={{
+                  hours: convertFloatTimeToHHMM(projectTime.hours),
+                  description: projectTime.description,
+                }}
+                recurringTasks={recurringTasks}
+                phaseTasks={phaseTasks}
+                calculationPosition={position}
+                disabled={disabled}
+              />
+            </div>
+          ))
+      )}
+    </div>
   ));
 }
